ff927a598e1069637b9d8f33e7c10e1d2fc211a5
kmmaruthi06@gmail.com <kmmaruthi06@gmail.com>
2018-10-12 02:00:44 +0530

    changes

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,12 +1,12 @@
 import * as functions from "firebase-functions";
-const twilio = require('twilio');
+const twilio = require('twilio').twiml.VoiceResponse;
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
   //response.send("Hello from Firebase! mkm test2");
-  const twiml = new twilio.TwimlResponse();
+  const twiml = new twilio();
   twiml.say("hello world!", { voice: "alice" });
   console.log(request.body)  
   // Render the response as XML in reply to the webhook request
