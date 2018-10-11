fab888d0bb4f8364f1a9536923b37d22ac5e9311
kmmaruthi06@gmail.com <kmmaruthi06@gmail.com>
2018-10-12 02:13:13 +0530

    test twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,5 +1,5 @@
 import * as functions from "firebase-functions";
-const twilio = require('twilio').twiml.VoiceResponse;
+const twilio = require("twilio").twiml.VoiceResponse;
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
@@ -7,8 +7,11 @@ const twilio = require('twilio').twiml.VoiceResponse;
 export const helloWorld = functions.https.onRequest((request, response) => {
   //response.send("Hello from Firebase! mkm test2");
   const twiml = new twilio();
-  twiml.say("hello world!", { voice: "alice" });
-  console.log(request.body)  
+
+  twiml.gather({ numDigits: 1 }, gatherNode => {
+    gatherNode.say("For sales, press 1. For support, press 2.");
+  });
+  console.log(request.body);
   // Render the response as XML in reply to the webhook request
   response.type("text/xml");
   response.send(twiml.toString());
