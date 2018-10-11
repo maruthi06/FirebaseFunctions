9e1d3375c9af6e0bec7f932626272620c3bf10ba
kmmaruthi06@gmail.com <kmmaruthi06@gmail.com>
2018-10-12 01:52:05 +0530

    twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,15 @@
 import * as functions from "firebase-functions";
+const twilio = require('twilio');
 
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
 export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase! mkm test2");
+  //response.send("Hello from Firebase! mkm test2");
+  const twiml = new twilio.TwimlResponse();
+  twiml.say("hello world!", { voice: "alice" });
+  console.log(request.body)  
+  // Render the response as XML in reply to the webhook request
+  response.type("text/xml");
+  response.send(twiml.toString());
 });
