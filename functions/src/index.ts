64f1532c3e8d9740d0dd19a12a03f5cdec6aa52d
kmmaruthi06@gmail.com <kmmaruthi06@gmail.com>
2018-10-12 02:29:24 +0530

    test twilio

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,9 +8,27 @@ export const helloWorld = functions.https.onRequest((request, response) => {
   //response.send("Hello from Firebase! mkm test2");
   const twiml = new twilio();
 
-  twiml.gather({ numDigits: 1 }, gatherNode => {
-    gatherNode.say("For sales, press 1. For support, press 2.");
-  });
+  let gather = twiml.gather({ numDigits: 1 });
+  gather.say("For sales, press 1. For support, press 2.");
+  if (request.body.Digits) {
+    switch (request.body.Digits) {
+      case "1":
+        gather = twiml.gather({ input: "speech" });
+        twiml.say("You selected sales. Good for you!");
+        break;
+      case "2":
+        gather = twiml.gather({ input: "speech" });
+        twiml.say("You need support. We will help!");
+        break;
+      default:
+        twiml.say("Sorry, I don't understand that choice.").pause();
+        gather();
+        break;
+    }
+  } else {
+    // If no input was sent, use the <Gather> verb to collect user input
+    gather();
+  }
   console.log(request.body);
   // Render the response as XML in reply to the webhook request
   response.type("text/xml");
